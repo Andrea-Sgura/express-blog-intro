9d4cd13d696f3637938de8c90e16b2e3ecaa2881
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-16 16:56:05 +0200

    [UPD] Eseguita importazione di express, inizializzazione nella variabile app, definizione del numero di porta, definizione della rotta base.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(`Hello World`);
+// Importazione di express
+const express = require(`express`);
+
+// Inizializzare express nella variabile app
+const app = express();
+
+// Definizione del numero di porta
+const port = 3000;
+
+// Definizione della rotta base
+app.get(`/`, (req, res) => {
+    console.log(`Hello World`)
+})
+
+// Inserimento del metodo che lascia in ascolto il nostro server
+app.listen(port, () => {
+    console.log(`Server del blog in ascolto alla porta ${port}`)
+})
